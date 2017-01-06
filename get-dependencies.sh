5bf434b9b874d7daa740d71001e6764dd8f0d0e4
juju4 <juju4@users.noreply.github.com>
2017-01-06 14:55:56 -0500

    sync repositories - add Jenkinsfile, galaxy naming

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,4 +6,10 @@ umask 022
 rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/jdauphant.unbound ] && git clone https://github.com/jdauphant/ansible-role-unbound.git $rolesdir/jdauphant.unbound
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.dnscrypt-proxy ] && ln -s ansible-dnscrypt-proxy $rolesdir/juju4.dnscrypt-proxy
+[ ! -e $rolesdir/juju4.dnscrypt-proxy ] && cp -R $rolesdir/ansible-dnscrypt-proxy $rolesdir/juju4.dnscrypt-proxy
+
+## don't stop build on this script return code
+true
 
