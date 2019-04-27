575cf6a8e0aa31a811593ca2a3d582ea4e1362a5
juju4 <juju4@users.noreply.github.com>
2019-04-27 09:52:06 -0400

    travis+kitchen: separate test suite default-blacklist

---
+++ test/integration/default-blacklist/serverspec/dnscrypt_spec.rb
@@ -0,0 +1,28 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe service('dnscrypt-proxy'), :if => os[:family] == 'ubuntu' do  
+  it { should be_enabled   }
+  it { should be_running   }
+end  
+
+describe process("dnscrypt-proxy") do
+
+  its(:user) { should eq "root" }
+
+  it "is listening on port 53" do
+    expect(port(53)).to be_listening
+  end
+
+end
+
+describe port(53) do
+  it { should be_listening.on('127.0.0.2') }
+end
+
+describe file('/var/log/dnscrypt-proxy.err') do
+  its(:content) { should match /\[NOTICE\] dnscrypt-proxy is ready - live servers:/ }
+  its(:content) { should_not match /FATAL/ }
+end
